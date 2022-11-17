05d82deed549724aa252b2c28189a19f0c4e8ef9
ahmettopall <ahmettopal@alsay.com.tr>
2022-11-17 16:12:59 +0300

    add login screen ui

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+/* eslint-disable eqeqeq */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Chat Application</span>
+        <span className="title">Login</span>
+        <form>
+            <input type="email" placeholder="email" />
+            <input type="password" placeholder="password" />
+            <button>Sign in</button>
+        </form>
+        <p>You don't have an account? Register</p>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
